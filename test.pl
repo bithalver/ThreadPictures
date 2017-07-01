e7634f23e13fc8cfdc98c6cbd4714aa69b083085
Ultra Bithalver <bithalver@gmail.com>
2017-07-01 13:30:03 +0200

    adding test for handling TP_threads env var

--- test.pl
+++ test.pl
@@ -2,11 +2,14 @@ use strict;
 use warnings;
 
 use threadpictures_draw;
-use threadpictures_defaults;
+use threadpictures_global;
 
 
 draw_line(1,2,3,4);
 
-print "$TP_threads \n";
+print $TP_threads,"\n";
+# The 2 tests are:
+#    TP_threads=2 perl test.pl   # Output is 2
+#    perl test.pl                # Output is 10
 
 
