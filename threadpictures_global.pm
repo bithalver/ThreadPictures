6ced4ddf0d47a735aeef27ef4897768ad2dbdb60
Ultra Bithalver <bithalver@gmail.com>
2017-07-01 13:28:16 +0200

    TP_threads variable setup

--- threadpictures_global.pm
+++ threadpictures_global.pm
@@ -8,7 +8,8 @@ our @ISA= qw( Exporter );
 our @EXPORT = qw( $TP_threads );
 
 # TP_threads is how many segment should exist in a net
-our $TP_threads=10;
+# Could be overwritten with the same name env var
+our $TP_threads = $ENV{'TP_threads'} //=10;
 
 1;
 
