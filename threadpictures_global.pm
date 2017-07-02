2bab69c7ed6a90f298fb835f4ec3d3b06222c738
Ultra Bithalver <bithalver@gmail.com>
2017-07-02 11:21:28 +0200

    first and last thread global variable added

--- threadpictures_global.pm
+++ threadpictures_global.pm
@@ -5,11 +5,14 @@ use Exporter;
 
 our @ISA= qw( Exporter );
 
-our @EXPORT = qw( $TP_threads @TP_all $TP_style $TP_pagename);
+our @EXPORT = qw( $TP_threads @TP_all $TP_style $TP_pagename $TP_firstthread $TP_lastthread);
 
 # TP_threads is how many segment should exist in a net
 # Could be overwritten with the same name env var
 our $TP_threads = $ENV{'TP_threads'} //=10;
+our $TP_firstthread = $ENV{'TP_firstthread'} //=0;
+our $TP_lastthread = $ENV{'TP_lastthread'} //= -1; # -1 is a special flag meaning real last one
+
 
 # This variable collects all nets to be drawn on one page
 our @TP_all;
