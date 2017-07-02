22bccd6f1104e9a3f98bca00087368cd6e88c46f
Ultra Bithalver <bithalver@gmail.com>
2017-07-02 10:19:07 +0200

    pagename variable added

--- threadpictures_global.pm
+++ threadpictures_global.pm
@@ -5,7 +5,7 @@ use Exporter;
 
 our @ISA= qw( Exporter );
 
-our @EXPORT = qw( $TP_threads @TP_all $TP_style);
+our @EXPORT = qw( $TP_threads @TP_all $TP_style $TP_pagename);
 
 # TP_threads is how many segment should exist in a net
 # Could be overwritten with the same name env var
@@ -19,6 +19,10 @@ our @TP_all;
 # 1 is holes to make patterns for sewing
 # all other positive numbers are future expansion
 our $TP_style = $ENV{'TP_style'} //=0;
+
+# What should be written on the bottom of the page
+# Default is not to write anything
+our $TP_pagename = $ENV('TP_pagename'} //="";
 
 1;
 
