0d72f8deedae4e456ca03444dab807acc66ae40d
Laszlo Meszaros <laszlo.meszaros@sap.com>
2018-01-08 16:35:48 +0100

    testrun modified that way it accepts both 20180106 and input/20180106.yaml style

--- testrun.sh
+++ testrun.sh
@@ -6,8 +6,10 @@
 # example: 
 #    ./testrun.sh 20180104
 
-export INPUTFILE=input/$1.yaml;
-export OUTPUTFILE=output/$1.pdf;
+INPUT=`basename $1 .yaml`
+
+export INPUTFILE=input/$INPUT.yaml;
+export OUTPUTFILE=output/$INPUT.pdf;
 
 while sleep 1; do
   if [[ $INPUTFILE -nt $OUTPUTFILE ]]; then
