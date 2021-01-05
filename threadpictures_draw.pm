b9b88e4f2deb209a96d31801c6be47b6a6234662
Ultra Bithalver <bithalver@gmail.com>
2021-01-05 12:34:00 +0100

    for printing holes, the size of the crossing lines (the ladder size) was 1/20 of the whole line; this is 1/10 now for threads < 6. reason: by adding the same net with threads=2, toy can have halving points

--- threadpictures_draw.pm
+++ threadpictures_draw.pm
@@ -257,10 +257,11 @@ sub draw_net {
   }
   when (/^holes$/i) { # old style was 1
     # This kind is NOT interested in the value of firstthread, lastthread
-    # Cross lines are 1/20 of the full length of the line    
-    my ($firstX,$firstY)=TP_weight($AN{line1oX},$AN{line1oY},$AN{line1iX},$AN{line1iY},1,20);
+    # Cross lines are 1/20 of the full length of the line EXCEPT if threads<6 then they are 1/10
+    my $laddersize = $AN{'threads'} < 6 ? 10 : 20;
+    my ($firstX,$firstY)=TP_weight($AN{line1oX},$AN{line1oY},$AN{line1iX},$AN{line1iY},1,$laddersize);
     my ($line1vectorX,$line1vectorY) = ($firstY-$AN{line1oY}, $AN{line1oX}-$firstX);
-    ($firstX,$firstY)=TP_weight($AN{line2oX},$AN{line2oY},$AN{line2iX},$AN{line2iY},1,20);
+    ($firstX,$firstY)=TP_weight($AN{line2oX},$AN{line2oY},$AN{line2iX},$AN{line2iY},1,$laddersize);
     my ($line2vectorX,$line2vectorY) = ($firstY-$AN{line2oY}, $AN{line2oX}-$firstX);
 
     draw_line($AN{line1oX},$AN{line1oY},$AN{line1iX},$AN{line1iY});
