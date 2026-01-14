6dacbf76ee159d7eb58e2fcd27e6b847049c7c29
Ultra Bithalver <bithalver@gmail.com>
2026-01-14 21:57:36 +0100

    "," was deleted from accepted characters of default value

--- PP.pl
+++ PP.pl
@@ -6,7 +6,7 @@ no warnings 'uninitialized';
 
 while (my $input = <> ) {
   # while ($input =~ s!ENV\[(\S*?)\](\S*)!$ENV{$1} // $2!e) { }
-  while ($input =~ s!ENV\[(\w*?)\]([a-zA-Z0-9_.,-]*)!$ENV{$1} // $2!e) { }
+  while ($input =~ s!ENV\[(\w*?)\]([a-zA-Z0-9_.-]*)!$ENV{$1} // $2!e) { }
   print ($input);
 }
 
@@ -18,4 +18,4 @@ Preprocessor
 ENV[abc] will be replaced by the value of the env var 'abc'
 ENV[abc]def is the same, but: if env var 'abc' is not defined the default value is 'def'.
 env var name can contain only [a-zA-Z0-9_]
-default value can contain only [a-zA-Z0-9_.,-] ; set is expendable but never add the semicolon !
+default value can contain only [a-zA-Z0-9_.-] ; set is expendable but never add the following: ",|;" !
