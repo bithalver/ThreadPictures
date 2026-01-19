88da89866efe7aea47f53e7514e1f4987e8a05f0
Ultra Bithalver <bithalver@gmail.com>
2026-01-19 11:59:43 +0100

    added named RANDOM; see PP.pl

--- PP.pl
+++ PP.pl
@@ -6,10 +6,13 @@ no warnings 'uninitialized';
 
 srand();
 
+my %RA; # RandomArray: named randoms are stored inside
+
 while (my $input = <> ) {
-  # while ($input =~ s!ENV\[(\S*?)\](\S*)!$ENV{$1} // $2!e) { }
-  while ($input =~ s!ENV\[(\w*?)\]([a-zA-Z0-9_.-]*)!$ENV{$1} // $2!ae) { }
-  no warnings 'numeric' ; while ($input =~ s!RANDOM\[([0-9.]*?)\]!rand($1)!ae) { }
+  $input =~ s!ENV\[(\w*?)\]([a-zA-Z0-9_.-]*)!$ENV{$1} // $2!aeg ;
+  no warnings 'numeric' ; $input =~ s!RANDOM\[([a-zA-Z][a-zA-Z0-9_-]*?),?([0-9.]*)\]!$ENV{$1} // $RA{$1} // ($RA{$1}=rand($2))!aeg ;
+  no warnings 'numeric' ; $input =~ s!RANDOM\[([0-9.]*?)\]!rand($1)!aeg ;
+#  print STDERR $input;
   print ($input);
 }
 
@@ -23,6 +26,12 @@ ENV[abc]def is the same, but: if env var 'abc' is not defined the default value 
 env var name can contain only [a-zA-Z0-9_]
 default value can contain only [a-zA-Z0-9_.-] ; set is expendable but never add the following: ",|;" !
 
-RANDOM[abc] will be replaced by a random value
-  if abc is given, value is 0<= x < abc
-  if not, value is 0< x < 1
+RANDOM[value] will be replaced by a random value
+  if value is given, return value is 0<= x < abc
+  if not, return value is 0< x < 1
+
+RANDOM[name,value] will be replaced by a random value _and_ stored in the hash %RA with name
+  environment variable with same name always overrides
+  if the name already got a random, value is discarded
+  name has to start with a letter and can contain [a-zA-Z0-9_-]
+  value can contain only [0-9.]
