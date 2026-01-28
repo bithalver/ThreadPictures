1af88aba91a1115c564d46f4d80f1471bc58f1e5
Ultra Bithalver <bithalver@gmail.com>
2026-01-28 16:22:12 +0100

    fine tuning ENV and RANDOM

--- PP.pl
+++ PP.pl
@@ -9,10 +9,10 @@ srand();
 my %RA; # RandomArray: named randoms are stored inside
 
 while (my $input = <> ) {
-#  print STDERR $input;
-  no warnings 'numeric' ; $input =~ s!ENV\[([a-zA-Z][a-zA-Z0-9_-]*),([0-9.\-_ a-zA-Z ,]+)\]!$ENV{$1} // $2!aeg ;
+  print STDERR "< ",$input;
+  no warnings 'numeric' ; $input =~ s!ENV\[([a-zA-Z][a-zA-Z0-9_-]*),?([0-9.\-_ a-zA-Z ,]*?)\]!$ENV{$1} // ($2 eq "" ? 1 : $2)!aeg ;
   no warnings 'numeric' ; $input =~ s!RANDOM\[([a-zA-Z][a-zA-Z0-9_-]*),?([0-9.]*)\]!$ENV{$1} // $RA{$1} // ($RA{$1}=int(rand($2) * 10**6) / 10**6)!aeg ;
-#  print STDERR $input;
+  print STDERR "> ",$input;
   print ($input);
 }
 
@@ -23,13 +23,14 @@ exit 0 ;
 Preprocessor
 
 ENV[abc,def] will be replaced by the value of the env var 'abc', if that ENV var exists, def, otherwise
-  both parameters are mandatory
+  name is mandatory, value (and the comma before it) is optional
   env var name has to start with a letter and can contain [a-zA-Z0-9_-]
   default value can contain only [0-9.\-_ a-zA-Z ,] ; set is expendable but never add the following: "|;" !
+  if there is no default value and not env var exists, value will be 1.
 
 RANDOM[name,value] will be replaced by a random value _and_ stored in the hash %RA with name
-  name is mandatory, value is optional
-  environment variable with same name always overrides with hihest priority
+  name is mandatory, value (and the comma before it) is optional
+  environment variable with same name always overrides with highest priority
   if the name already got a random, value is discarded
   generates a random number between 0<= x < value ; if value is omitted, end is '1'
   name has to start with a letter and can contain [a-zA-Z0-9_-]
